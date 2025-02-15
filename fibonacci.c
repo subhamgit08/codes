740dc896bfa6278d1e63a7371399c7e5353017b5
Swapneel14 <sarkarswapneel14@gmail.com>
2025-02-15 21:53:04 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    int f0=0;
+    int f1=1;
+    printf("%d ",f0);
+    printf("%d ",f1);
+    int f2;
+    for(int i=2;i<=n;i++){
+       f2=f1+f0;
+       printf("%d ",f2);
+       f1=f2;
+       f0=f1;
+    }
+    
+}
