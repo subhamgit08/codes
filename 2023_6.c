d14c6483db1f14f976da8806a37d8a520cf410db
subhamgit08 <subham08.dev@gmail.com>
2025-02-16 10:36:56 +0530

    .

--- 2023_6.c
+++ 2023_6.c
@@ -1,17 +1,23 @@
-#include<stdio.h>
-void main(){
+#include <stdio.h>
+void main()
+{
     int n;
     int s;
-    int result=0;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        s=i*i;
-       if(i%2==0){
-        printf("%d",0-s);
-       }
-       else{
-        printf("+%d",s);
-       }
-
+    int result = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        s = i * i;
+        if (i % 2 == 0)
+        {
+            printf("%d", 0 - s);
+            result -= s;
+        }
+        else
+        {
+            printf("+%d", s);
+            result += s;
+        }
     }
+    printf("\n%d\n",result);
 }
