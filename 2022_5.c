f71ac000e7cdf992236bc63566aa8c7a685a4bea
subhamgit08 <subham08.dev@gmail.com>
2025-02-27 00:44:36 +0530

    .

--- 2022_5.c
+++ 2022_5.c
@@ -1,16 +1,27 @@
-#include<stdio.h>
-void main(){
+#include <stdio.h>
+void main()
+{
     int n;
-    scanf("%d",&n);
-    int f0=0;
-    int f1=1;
-    printf("%d ",f0);
-    printf("%d ",f1);
-    int f2;
-    for(int i=2;i<=n;i++){
-       f2=f1+f0;
-       printf("%d ",f2);
-       f1=f2;
-       f0=f1;
-    }    
+    scanf("%d", &n);
+    int f0 = 0;
+    int f1 = 1;
+    if (n > 0)
+    {
+        printf("%d ", f0);
+    }
+    if (n > 1)
+    {
+        printf("%d ", f1);
+    }
+    if (n > 2)
+    {
+        int f2;
+        for (int i = 2; i < n; i++)
+        {
+            f2 = f1 + f0;
+            printf("%d ", f2);
+            f0 = f1;
+            f1 = f2;
+        }
+    }
 }
