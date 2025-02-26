c17b013460d5134d28119d6e8dd6ac3edd320104
subhamgit08 <subham08.dev@gmail.com>
2025-02-26 22:25:47 +0530

    .

--- 2024_4b.c
+++ 2024_4b.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int n,x, sum = 1;
+    int n,x, sum = 1,power =1;
     printf("Enter the number of terms\n");
     scanf("%d",&n);
     printf("Enter the value of x\n");
     scanf("%d",&x);
     for (int i = 0; i < n-1; i++)
     {
-        int temp = x;
-        for (int j = 0; j < i; j++)
-        {
-            temp *= x;
-        }
-        sum += temp;
+        power *= x;
+        sum += power;
     }
     printf("The output is : %d\n",sum);
     return 0;
