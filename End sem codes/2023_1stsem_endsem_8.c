f3042616a0390c8c60dbe3e33709e1561f4ae18c
Your GitHub Username <your.email@example.com>
2025-04-28 11:34:59 +0530

    .

--- End sem codes/2023_1stsem_endsem_8.c
+++ End sem codes/2023_1stsem_endsem_8.c
@@ -45,7 +45,7 @@ int main(){
     char name[50];
     printf("Enter the numbers of students: ");
     scanf("%d",&n);
-    getchar();
+    // getchar();
     file = fopen("myfile.txt","w");
     if (file == NULL){
         printf("Error opening file!\n");
@@ -54,14 +54,15 @@ int main(){
     for(int i=0;i<n;i++){
         printf("Student %d:\n",i+1);
         printf("Enter the name: ");
-        fgets(name,50,stdin);
+        // fgets(name,50,stdin);
+        scanf("%s",name);
         printf("Enter the roll no: ");
         scanf("%d",&roll);
         printf("Enter the marks: ");
         scanf("%d",&marks);
-        getchar();
+        // getchar();
         fprintf(file,"Student %d:\n",i+1);
-        fprintf(file," Name: %s",name);
+        fprintf(file," Name: %s\n",name);
         fprintf(file," Roll No.: %d\n Marks: %d\n",roll,marks);
     }
     fclose(file);
